a9b7441a6efdecea4ed6c4f6b27e8dcd43e6d675
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-26 14:54:57 +0530

    Add pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import MeetupList from "@/components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "Vipul's home",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvcjK_db1pjcqBVZq37ryXZRU-S2cMEdsrPQ&usqp=CAU",
+    address: "some address , some city, some state",
+    description: "First meetup",
+  },
+  {
+    id: "m2",
+    title: "kathi's home",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuIfBFC3FU9DC9MEIClYYsU1XLJIglrwIiBg&usqp=CAU",
+    address: "some address , some city, some state",
+    description: "Second meetup",
+  },
+];
+
 const HomePage = () => {
-    return <h1>Hello world</h1>
-}
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
 
 export default HomePage;
