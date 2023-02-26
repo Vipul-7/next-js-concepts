7a8a73cfc0347b0295bb98ab231a216fed02a3fe
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-26 18:55:28 +0530

    Worked with SSG(static site generation) and SSR

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 import MeetupDetails from "@/components/meetups/MeetupDetails";
 
-const MeetupDetailsPage = () => {
+const MeetupDetailsPage = (props) => {
   return (
     <MeetupDetails
       title="Vipul's home"
@@ -12,3 +12,40 @@ const MeetupDetailsPage = () => {
 };
 
 export default MeetupDetailsPage;
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: "Vipul's home",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvcjK_db1pjcqBVZq37ryXZRU-S2cMEdsrPQ&usqp=CAU",
+        address: "some address , some city, some state",
+        description: "First meetup",
+      },
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
