a96d7073ff7ee29694dae1f88f4aa80a0e5d0ae1
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-26 15:22:26 +0530

    meetup details component added

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetails from "@/components/meetups/MeetupDetails";
+
+const MeetupDetailsPage = () => {
+  return (
+    <MeetupDetails
+      title="Vipul's home"
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvcjK_db1pjcqBVZq37ryXZRU-S2cMEdsrPQ&usqp=CAU"
+      address="some address , some city, some state"
+      description="First meetup"
+    />
+  );
+};
+
+export default MeetupDetailsPage;
