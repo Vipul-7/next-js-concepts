5f0b5f94547eaffa6fca765b7c37c2e30b5cdc02
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-26 19:48:19 +0530

    made my own api route

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://vips:Mongodb123@cluster0.xfiaguw.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollections = db.collection("meetups");
+
+    const result = await meetupsCollections.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({message:'Meetup inserted.'})
+  }
+};
+
+export default handler;
