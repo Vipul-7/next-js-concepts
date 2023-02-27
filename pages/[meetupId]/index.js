4d9bcbb966d9c26715c9da25651b17be2000afc0
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-27 22:22:46 +0530

    Added meta data with next.js's Head tag

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,21 @@
 import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetails from "@/components/meetups/MeetupDetails";
+import Head from "next/head";
 
 const MeetupDetailsPage = (props) => {
   return (
-    <MeetupDetails
-      title={props.meetupData.title}
-      image={props.meetupData.image}
-      address={props.meetupData.address}
-      description={props.meetupData.description}
-    />
+    <>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head> 
+      <MeetupDetails
+        title={props.meetupData.title}
+        image={props.meetupData.image}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </>
   );
 };
 
